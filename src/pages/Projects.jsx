b45de1faf7af6e99abc117128d48bfd40fecaa27
Deivy Munoz <dmunoz2460@gmail.com>
2025-07-09 17:20:29 -0400

    add projects information

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,49 +3,35 @@ import ProjectCard from '../components/ProjectCard'
 
 const projectData = [
   {
-    title: 'ESP32 Temperature Logger',
-    description: 'Reads temperature and humidity using DHT11, displays data on OLED, and publishes via MQTT.',
-    tech: 'ESP32, DHT11, MQTT, OLED, Arduino',
-    github: 'https://github.com/yourusername/esp32-logger',
-    demoPage: 'https://github.com/yourusername/esp32-logger'
-  },
-  {
-    title: 'Motor Control with PWM',
-    description: 'Controls DC motor speed and direction using PWM and H-bridge driver.',
-    tech: 'STM32, C, PWM, Motor Driver, FreeRTOS',
-    github: 'https://github.com/yourusername/esp32-logger',
-    demoPage: 'https://github.com/yourusername/esp32-logger'
-  },
-  {
-    title: 'Custom PCB Signal Generator',
-    description: 'Twin-T oscillator circuit designed in KiCad and prototyped to generate a 22kHz signal.',
-    tech: 'KiCad, Op-Amps, Oscillator Design, Analog Electronics',
-    github: 'https://github.com/yourusername/esp32-logger',
-    demoPage: 'https://github.com/yourusername/esp32-logger'
+    title: 'Iris Biometrics Recognition',
+    description: 'This research created a new iris image dataset using white light to improve recognition for Latin-American eyes. By adapting the EigenIris method and analyzing RGB channels, especially blue, the study achieved strong accuracy without infrared.',
+    tech: 'Python, OpenCV, Biometrics Recognition, Image Processing',
+    github: 'https://github.com/DeivyAlejo/iris-biometric-recognition',
+    demoPage: 'https://revistas.utp.edu.co/index.php/revistaciencia/article/view/22861/16382'
   },
   {
     title: 'ESP32 Temperature Logger',
     description: 'Reads temperature and humidity using DHT11, displays data on OLED, and publishes via MQTT.',
-    tech: 'ESP32, DHT11, MQTT, OLED, Arduino',
-    github: 'https://github.com/yourusername/esp32-logger',
+    tech: 'ESP32, DHT11, MQTT, OLED, Arduino, I2C',
+    github: 'https://github.com/DeivyAlejo/temp_sensor_esp32',
     demoPage: 'https://github.com/yourusername/esp32-logger'
   },
   {
-    title: 'Motor Control with PWM',
-    description: 'Controls DC motor speed and direction using PWM and H-bridge driver.',
-    tech: 'STM32, C, PWM, Motor Driver, FreeRTOS',
-    github: 'https://github.com/yourusername/esp32-logger',
-    demoPage: 'https://github.com/yourusername/esp32-logger'
+    title: 'Word Search Solver',
+    description: 'This Python project solves word search puzzles by finding the sequence of letters that form a target word in the correct direction from a grid',
+    tech: 'Python, AWS, Lambda, API Gateway, Apmlify, Data Structures',
+    github: 'https://github.com/DeivyAlejo/word_search_solver',
+    demoPage: 'https://www.youtube.com/watch?v=PPkMyLJM1gI'
   },
   {
-    title: 'Custom PCB Signal Generator',
-    description: 'Twin-T oscillator circuit designed in KiCad and prototyped to generate a 22kHz signal.',
-    tech: 'KiCad, Op-Amps, Oscillator Design, Analog Electronics',
-    github: 'https://github.com/yourusername/esp32-logger',
-    demoPage: 'https://github.com/yourusername/esp32-logger'
-  }
+    title: 'Video Cropper',
+    description: 'This script lets users select and crop a video region using mouse clicks, then saves the cropped output.',
+    tech: 'OpenCV, Python, Tkinter',
+    github: 'https://github.com/DeivyAlejo/Video-Cropper',
+    demoPage: 'https://github.com/DeivyAlejo/Video-Cropper'
+  },
+  
 ]
-
 const Projects = () => {
   return (
     <div className="pt-20 min-h-screen text-black px-6">
