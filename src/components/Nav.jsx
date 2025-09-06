7dbbf4c5570ff9223e8c5a652233db0d63256e20
Deivy <dmunoz2460@gmail.com>
2025-09-06 17:14:30 -0400

    changing links on navbar and homepage

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,13 +6,13 @@ const Nav = ({ handleClick }) => {
     const buttonClasses = 'font-semibold text-lg px-4 py-1 border border-white hover:border-black rounded-full transition duration-300'
     return (
         <>
-            <NavLink to="/my-website/education" className={({ isActive }) =>
+            <NavLink to="/education" className={({ isActive }) =>
                 `${buttonClasses} ${isActive ? 'bg-black text-gray-100' : ''}`} onClick={handleClick}>Education</NavLink>
-            <NavLink to="/my-website/experience" className={({ isActive }) =>
+            <NavLink to="/experience" className={({ isActive }) =>
                 `${buttonClasses} ${isActive ? 'bg-black text-gray-100' : ''}`} onClick={handleClick}>Experience</NavLink>
-                <NavLink to="/my-website/projects" className={({ isActive }) =>
+                <NavLink to="/projects" className={({ isActive }) =>
                 `${buttonClasses} ${isActive ? 'bg-black text-gray-100' : ''}`} onClick={handleClick}>Projects</NavLink>
-            <NavLink to="/my-website/contact" className={({ isActive }) =>
+            <NavLink to="/contact" className={({ isActive }) =>
                 `${buttonClasses} ${isActive ? 'bg-black text-gray-100' : ''}`} onClick={handleClick}>Contact</NavLink>
         </>
     )
